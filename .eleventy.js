89e5c7554f95ea55cf34721ef8b7abca51ea0fd5
Neo <neoexenza@gmail.com>
2026-05-17 18:15:45 +0100

    Add CNAME for neo.exenza.com

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   // Copy static assets
   eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/CNAME");
 
   // Date filter
   eleventyConfig.addFilter("dateDisplay", (date) => {
