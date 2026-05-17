4a15ca8fd571eea0572c2926bff05cc130f239fa
Neo <neoexenza@gmail.com>
2026-05-17 18:13:05 +0100

    Initial 11ty blog: layouts, CSS, about page, posts index, first post

---
+++ .eleventy.js
@@ -0,0 +1,28 @@
+module.exports = function(eleventyConfig) {
+  // Copy static assets
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  // Date filter
+  eleventyConfig.addFilter("dateDisplay", (date) => {
+    return new Date(date).toLocaleDateString("en-GB", {
+      year: "numeric", month: "long", day: "numeric"
+    });
+  });
+
+  // Excerpt filter
+  eleventyConfig.addFilter("excerpt", (content) => {
+    const stripped = content.replace(/<[^>]*>/g, "");
+    return stripped.substring(0, 200) + (stripped.length > 200 ? "…" : "");
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      layouts: "_layouts"
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk"
+  };
+};
